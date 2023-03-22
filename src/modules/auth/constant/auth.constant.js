a04c12b92369c6c6c5ce444c101b8909ca1f2554
teguhbts <teguh.m@bts.id>
2023-03-22 21:45:51 +0700

    Feat: Menu by role

--- src/modules/auth/constant/auth.constant.js
+++ src/modules/auth/constant/auth.constant.js
@@ -1,3 +1,5 @@
+import { FiHome, FiTrendingUp, FiCompass, FiStar, FiSettings, FiMenu } from 'react-icons/fi';
+
 export const AUTH_ROLE_PERMISSIONS = {
   event: ['VIEW_EVENT', 'EDIT_EVENT'],
   tree: ['VIEW_TREE', 'EDIT_TREE'],
@@ -5,38 +7,83 @@ export const AUTH_ROLE_PERMISSIONS = {
   formula: ['VIEW_FORMULA', 'CREATE_FORMULA', 'CALCULATE_FORMULA'],
   formAdvance: ['VIEW_FORM_CANGGIH'],
   table: ['VIEW_TABLE', 'CREATE_CONFIG_TABLE', 'UPDATE_CONFIG_TABLE'],
-  graphic: ['VIEW_GRAPHIC']
+  graphic: ['VIEW_GRAPHIC'],
 };
 
-export const AUTH_USER = {
-  dimas: {
-    name: 'Dimas',
+export const AUTH_USERS = [
+  {
+    fullName: 'Dimas',
+    username: 'dimas',
     password: 'dimas123',
-    roles: ['event', 'tree']
+    roles: ['event', 'tree'],
   },
-  randy: {
-    name: 'Randy',
+  {
+    fullName: 'Randy',
+    username: 'randy',
     password: 'randy123',
-    roles: ['formUnique', 'formula']
+    roles: ['formUnique', 'formula'],
   },
-  adlan: {
-    name: 'Adlan',
+  {
+    fullName: 'Adlan',
+    username: 'adlan',
     password: 'adlan123',
-    roles: ['formAdvance', 'tabel']
+    roles: ['formAdvance', 'table'],
   },
-  kikoadmn: {
-    name: 'Kiko',
+  {
+    fullName: 'Kiko',
+    username: 'kikoadmn',
     password: 'kikoadmn',
-    roles: ['event', 'tree', 'formUnique', 'formula', 'formAdvance', 'table', 'graphic']
+    roles: ['event', 'tree', 'formUnique', 'formula', 'formAdvance', 'table', 'graphic'],
   },
-  admin: {
-    name: 'Admin',
+  {
+    fullName: 'Admin',
+    username: 'admin',
     password: 'admin',
-    roles: ['event', 'tree', 'formUnique', 'formula', 'formAdvance', 'table', 'graphic']
+    roles: ['event', 'tree', 'formUnique', 'formula', 'formAdvance', 'table', 'graphic'],
   },
-  parman: {
-    name: 'Parman',
+  {
+    fullName: 'Parman',
+    username: 'parman',
     password: 'parman',
-    roles: []
-  }
-};
+    roles: [],
+  },
+];
+
+export const MENUS = [
+  {
+    name: 'Jadwal',
+    icon: FiHome,
+    path: '/event',
+    access: 'event',
+  },
+  {
+    name: 'Silsilah Keluarga',
+    icon: FiTrendingUp,
+    path: '/family-tree',
+    access: 'tree',
+  },
+  {
+    name: 'Form Unik',
+    icon: FiCompass,
+    path: '/form-unique',
+    access: 'formUnique',
+  },
+  {
+    name: 'Formula',
+    icon: FiStar,
+    path: '/formula',
+    access: 'formula',
+  },
+  {
+    name: 'Form Canggih',
+    icon: FiSettings,
+    path: '/form-advance',
+    access: 'formAdvance',
+  },
+  {
+    name: 'Tabel 4.0',
+    icon: FiMenu,
+    path: '/table',
+    access: 'table',
+  },
+];
