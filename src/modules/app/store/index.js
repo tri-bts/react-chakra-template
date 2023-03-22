7d960a569307328188a89be0eb29d9c4d56c650a
Teguh Muhammad Zundi <teguh.m@bts.id>
2023-03-22 11:34:00 +0700

    Feat: added redux and mock db

---
+++ src/modules/app/store/index.js
@@ -0,0 +1,28 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+
+// Reducers
+import authReducer from '../../../modules/auth/slice/auth.slice';
+// Other Reducers ....
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  auth: persistReducer(authPersistConfig, authReducer),
+
+  // auth: authReducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
