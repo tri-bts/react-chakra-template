df87b95cb2e812417615be25277917c04b4763e2
Tri Sutrisno <128479557+tri-bts@users.noreply.github.com>
2023-03-23 13:19:32 +0700

    feat: event add / delete (#18)
    * build: add fullcalendar interaction and timegrid
    
    * build: add fullcalendar interaction and timegrid
    
    * Update package-lock.json
    
    * feat(event) add modal input
    
    * feat(event): add event
    
    * feat(event): delete event

---
+++ src/modules/event/components/EventInput.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  Button,
+  ModalBody,
+  ModalCloseButton,
+  Input,
+  Stack,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  VStack,
+} from '@chakra-ui/react';
+import { Formik } from 'formik';
+
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required().label('title'),
+});
+
+function EventInput({ isOpen, onClose, onAddEvent }) {
+  const handleClose = () => {
+    onClose();
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={handleClose} size="3xl">
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Add Event</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody pb={6}>
+          <Stack spacing={3}>
+            <Formik
+              initialValues={{ title: '' }}
+              validationSchema={validationSchema}
+              onSubmit={onAddEvent}>
+              {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+              }) => (
+                <form onSubmit={handleSubmit}>
+                  <VStack spacing={4} align="flex-start">
+                    <FormControl id="title" isInvalid={!!errors.title && touched.title}>
+                      <FormLabel>Title</FormLabel>
+                      <Input
+                        type="text"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.title}
+                      />
+                      <FormErrorMessage>{errors.title}</FormErrorMessage>
+                    </FormControl>
+                    <FormControl id="start" isInvalid={!!errors.start && touched.start}>
+                      <FormLabel>Start Date</FormLabel>
+                      <Input
+                        type="datetime-local"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.start}
+                      />
+                      <FormErrorMessage>{errors.start}</FormErrorMessage>
+                    </FormControl>
+                    <FormControl id="end" isInvalid={!!errors.End && touched.End}>
+                      <FormLabel>End Date</FormLabel>
+                      <Input
+                        type="datetime-local"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.End}
+                      />
+                      <FormErrorMessage>{errors.End}</FormErrorMessage>
+                    </FormControl>
+                    <Stack spacing={6}>
+                      <Button type="submit" disabled={isSubmitting} colorScheme="telegram" mr={3}>
+                        Add
+                      </Button>
+                    </Stack>
+                  </VStack>
+                </form>
+              )}
+            </Formik>
+          </Stack>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export default EventInput;
--- src/modules/event/pages/EventScreen.jsx
+++ src/modules/event/pages/EventScreen.jsx
@@ -1,27 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import FullCalendar from '@fullcalendar/react'; // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 import { useDispatch } from 'react-redux';
+
+import { Container, useDisclosure } from '@chakra-ui/react';
 
 import { event_fetch } from '../slice/event.slice';
 
-function handleDateClick(arg) {
-  console.log(arg);
-}
+import EventInput from '../components/EventInput';
 
 // a custom render function
 function renderEventContent(eventInfo) {
   return (
     <>
-      <b>{eventInfo.timeText}</b>
+      <b>{eventInfo.timeText} </b>
       <i>{eventInfo.event.title}</i>
     </>
   );
 }
 
 function EventScreen() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const dispatch = useDispatch();
   const [events, setEvents] = useState([]);
+
+  function handleDateClick() {
+    onOpen();
+  }
+
+  function handleEventClick(clickInfo) {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove();
+    }
+  }
+
+  function onAddEvent(values) {
+    setEvents(events => [...events, values]);
+    onClose();
+  }
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(async () => {
@@ -30,15 +48,23 @@ function EventScreen() {
   }, [dispatch]);
 
   return (
-    <div>
+    <Container maxW={'5xl'} py={12}>
       <FullCalendar
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay',
+        }}
         initialView="dayGridMonth"
         dateClick={handleDateClick}
         events={events}
+        eventClick={handleEventClick}
         eventContent={renderEventContent}
       />
-    </div>
+
+      {onOpen && <EventInput isOpen={isOpen} onClose={onClose} onAddEvent={onAddEvent} />}
+    </Container>
   );
 }
 
