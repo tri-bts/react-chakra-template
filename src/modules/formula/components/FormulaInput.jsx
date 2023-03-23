84cfc583f06b7235f31a8715577d3c44c3457cbe
teguhbts <teguh.m@bts.id>
2023-03-23 14:54:52 +0700

    Fix: Calculator

--- src/modules/formula/components/FormulaInput.jsx
+++ src/modules/formula/components/FormulaInput.jsx
@@ -1,13 +1,25 @@
 import { Input, InputGroup, InputRightAddon, Text } from '@chakra-ui/react';
 import { BiCalculator } from 'react-icons/bi';
+import FormulaCalculator from '@/modules/formula/components/FormulaCalculator';
+import { useCallback, useMemo, useState } from 'react';
 
 const FormulaInput = ({ label = '', formula = '' }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const propsMemo = useMemo(() => ({ label, formula }), [formula, label]);
+
+  const onSubmit = useCallback(values => {
+    console.log('onSubmit', values);
+  }, []);
+
   return (
     <>
+      <FormulaCalculator isOpen={isOpen} {...propsMemo} onSubmit={onSubmit} />
+
       <Text mb={2}>{label}</Text>
       <InputGroup>
         <Input value={formula} readOnly />
-        <InputRightAddon>
+        <InputRightAddon onClick={() => setIsOpen(!isOpen)}>
           <BiCalculator />
         </InputRightAddon>
       </InputGroup>
