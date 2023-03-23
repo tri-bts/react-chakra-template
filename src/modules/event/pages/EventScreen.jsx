150a4907ab4e0d6110029aa15d58a80d9152c37a
Tri Sutrisno <128479557+tri-bts@users.noreply.github.com>
2023-03-23 14:04:35 +0700

    feat : event (detail/delete) (#19)
    * build: add fullcalendar interaction and timegrid
    
    * build: add fullcalendar interaction and timegrid
    
    * Update package-lock.json
    
    * feat(event) add modal input
    
    * feat(event): add event
    
    * feat(event): delete event
    
    * feat(event) : show event detail
    
    - fix set id on create event
    
    * feat(event) : delete event
    
    * Update EventScreen.jsx

--- src/modules/event/pages/EventScreen.jsx
+++ src/modules/event/pages/EventScreen.jsx
@@ -4,12 +4,14 @@ import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 
 import { Container, useDisclosure } from '@chakra-ui/react';
 
 import { event_fetch } from '../slice/event.slice';
 
 import EventInput from '../components/EventInput';
+import EventDetail from '../components/EventDetail';
 
 // a custom render function
 function renderEventContent(eventInfo) {
@@ -23,20 +25,33 @@ function renderEventContent(eventInfo) {
 
 function EventScreen() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    isOpen: isOpenEventDetail,
+    onOpen: onOpenEventDetail,
+    onClose: onCloseEventDetail,
+  } = useDisclosure();
+
   const dispatch = useDispatch();
   const [events, setEvents] = useState([]);
+  const [event, setEvent] = useState({});
 
   function handleDateClick() {
     onOpen();
   }
 
   function handleEventClick(clickInfo) {
-    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-      clickInfo.event.remove();
+    setEvent(clickInfo.event);
+    onOpenEventDetail();
+  }
+  function removeEvent() {
+    onCloseEventDetail();
+    if (confirm(`Are you sure you want to delete the event '${event.title}'`)) {
+      event.remove();
     }
   }
 
   function onAddEvent(values) {
+    values.id = uuidv4();
     setEvents(events => [...events, values]);
     onClose();
   }
@@ -64,6 +79,14 @@ function EventScreen() {
       />
 
       {onOpen && <EventInput isOpen={isOpen} onClose={onClose} onAddEvent={onAddEvent} />}
+      {onOpenEventDetail && (
+        <EventDetail
+          isOpen={isOpenEventDetail}
+          onClose={onCloseEventDetail}
+          event={event}
+          removeEvent={removeEvent}
+        />
+      )}
     </Container>
   );
 }
